f1604f259a3940a7cdfbd1e4bb27dba63aeab64c
ZYM1111 <53007085+ZYM1111@users.noreply.github.com>
2023-03-30 21:13:32 +0800

    fix(graph/code/tree-hash/tree-hash_1.cpp): 交换做法一和二

--- docs/graph/code/tree-hash/tree-hash_1.cpp
+++ docs/graph/code/tree-hash/tree-hash_1.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 
 typedef unsigned long long ull;
-typedef std::pair<ull, ull> Hash2;
 
 const int N = 60, M = 998244353;
 const ull mask = std::chrono::steady_clock::now().time_since_epoch().count();
@@ -20,63 +19,46 @@ ull shift(ull x) {
   return x;
 }
 
-int n;
-int size[N], weight[N], centroid[2];
 std::vector<int> edge[N];
-std::map<Hash2, int> trees;
+ull sub[N], root[N];
+std::map<ull, int> trees;
 
-void getCentroid(int x, int fa) {
-  size[x] = 1;
-  weight[x] = 0;
+void getSub(int x) {
+  sub[x] = 1;
   for (int i : edge[x]) {
-    if (i == fa) {
-      continue;
-    }
-    getCentroid(i, x);
-    size[x] += size[i];
-    weight[x] = std::max(weight[x], size[i]);
-  }
-  weight[x] = std::max(weight[x], n - size[x]);
-  if (weight[x] <= n / 2) {
-    int index = centroid[0] != 0;
-    centroid[index] = x;
+    getSub(i);
+    sub[x] += shift(sub[i]);
   }
 }
 
-ull getHash(int x, int fa) {
-  ull hash = 1;
+void getRoot(int x) {
   for (int i : edge[x]) {
-    if (i == fa) {
-      continue;
-    }
-    hash += shift(getHash(i, x));
+    root[i] = sub[i] + shift(root[x] - shift(sub[i]));
+    getRoot(i);
   }
-  return hash;
 }
 
 int main() {
   int m;
   scanf("%d", &m);
   for (int t = 1; t <= m; t++) {
+    int n, rt = 0;
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
       int fa;
       scanf("%d", &fa);
       if (fa) {
         edge[fa].push_back(i);
-        edge[i].push_back(fa);
+      } else {
+        rt = i;
       }
     }
-    getCentroid(1, 0);
-    Hash2 hash;
-    hash.first = getHash(centroid[0], 0);
-    if (centroid[1]) {
-      hash.second = getHash(centroid[1], 0);
-      if (hash.first > hash.second) {
-        std::swap(hash.first, hash.second);
-      }
-    } else {
-      hash.second = hash.first;
+    getSub(rt);
+    root[rt] = sub[rt];
+    getRoot(rt);
+    ull hash = 1;
+    for (int i = 1; i <= n; i++) {
+      hash += shift(root[i]);
     }
     if (!trees.count(hash)) {
       trees[hash] = t;
@@ -85,6 +67,5 @@ int main() {
     for (int i = 1; i <= n; i++) {
       edge[i].clear();
     }
-    centroid[0] = centroid[1] = 0;
   }
 }
